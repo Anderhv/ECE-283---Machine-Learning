e02d73280cd59d7c8e35fad3ccb3e69d1f55b56a
AndersHV <anderhv@stud.ntnu.no>
2018-04-16 17:34:11 -0700

    Set decent values for l and lambda

--- Homework 1/classifyByMAP.m
+++ Homework 1/classifyByMAP.m
@@ -1,14 +1,30 @@
-function [t] = classifyByMAP(X)
+function [t_hat] = classifyByMAP(X)
+
+N = length(X);
 
 p_0 = getProbability(X, 0);
 p_1 = getProbability(X, 1);
-
-for i=1:size(X,2)
+for i=1:length(X)
     if (log(p_0(i)) > log(p_1(i)))
-        t(i) = 0;
+        t_hat(i) = 0;
     else
-        t(i) = 1;
+        t_hat(i) = 1;
     end
 end
+
+% Check which samples were correctly classified
+corr_0 = 0;
+corr_1 = 0;
+for i=1:N
+    if (i<=N/2 && t_hat(i) == 0)
+        corr_0 = corr_0 + 1;
+    elseif (i>N/2 && t_hat(i) == 1)
+        corr_1 = corr_1 + 1;
+    end 
+end
+% t_hat denotes the estimated label, t denotes the actual label
+fprintf('P(t_hat = 1 | t = 0) using MAP: %.3f\n',(N/2 - corr_0)/(N/2))
+fprintf('P(t_hat = 0 | t = 1) using MAP: %.3f\n',(N/2 - corr_1)/(N/2))
+
 
 end
--- Homework 1/main.m
+++ Homework 1/main.m
@@ -11,7 +11,7 @@ u_02 = [-sin(theta_0), cos(theta_0)]';
 C_0 = lambda_01*(u_01*u_01') + lambda_02*(u_02*u_02');
 
 rng(100)
-data_0 = mvnrnd(m_0, C_0, N)';
+X_0 = mvnrnd(m_0, C_0, N)';
 
 % Class 1
 % Component A
@@ -35,13 +35,13 @@ pi_1B = 2/3;
 % Mixture
 dist_1 = gmdistribution([m_1A'; m_1B'], cat(3,C_1A,C_1B),[pi_1A, pi_1B]);
 
-data_1 = random(dist_1,N)';
+X_1 = random(dist_1,N)';
 
 % Plot the generated data
 figure(1); clf
-scatter(data_0(1,:), data_0(2,:),'+')
+scatter(X_0(1,:), X_0(2,:),'.b')
 hold on
-scatter(data_1(1,:), data_1(2,:),'+r')
+scatter(X_1(1,:), X_1(2,:),'.r')
 legend('Class 0', 'Class 1')
 title('200 samples generated from each class')
 
@@ -50,16 +50,15 @@ save params.mat m_0 m_1A m_1B C_0 C_1A C_1B pi_1A pi_1B
 
 %% 2) Use MAP to classify samples and compute the decision boundary
 
-data = [data_0, data_1];
 % Classify samples according to maximum likelihood
-t = classifyByMAP(data); % t is a vector containing the labels of all samples
+t = classifyByMAP([X_0 X_1]); % t is a vector containing the labels of all samples
 
 % Define the ranges and resolution of the grid used for computing the
 % decision boundary
-u = linspace(-10, 10, 100);
-v = linspace(-10, 10, 100);
-
-zp = zeros(length(u), length(v));
+res = 150;
+u = linspace(-8, 8, res);
+v = linspace(-8, 8, res);
+z_MAP = zeros(res, res);
 
 % Evaluate zp = p_0 - p_1 over the grid
 for i = 1:length(u)
@@ -68,105 +67,67 @@ for i = 1:length(u)
     end
     p_uv_0  = getProbability(x, 0);
     p_uv_1  = getProbability(x, 1);
-    zp(:,i) = p_uv_0 - p_uv_1;
+    z_MAP(:,i) = p_uv_0 - p_uv_1;
 end
 
 % Plot the decision boundary with the samples
 figure(2);
-scatter(data_0(1,:), data_0(2,:),'+b')
+scatter(X_0(1,:), X_0(2,:),'.b')
 hold on
-scatter(data_1(1,:), data_1(2,:),'+r')
+scatter(X_1(1,:), X_1(2,:),'.r')
 hold on
-contour(u,v,zp, [0, 0],'k', 'LineWidth', 2)
+contour(u,v,z_MAP, [0, 0],'k', 'LineWidth', 2)
 title('Decision boundary using the MAP rule')
 legend('Class 0','Class 1','Decision boundary')
 
 %% 3) Estimation of conditional probability of incorrect classification with MAP
 
-N_test = 300;
-rng(100)
-Test_X_0 = mvnrnd(m_0, C_0, N_test)';
-Test_X_1 = random(dist_1,N_test)';
-
-data = [Test_X_0, Test_X_1];
-t = classifyByMAP(data);
-
-% Check which samples were correctly classified
-corr_0 = 0;
-corr_1 = 0;
-for i=1:size(t,2)
-    if (i<=N_test && t(i) == 0)
-        corr_0 = corr_0 + 1;
-    elseif (i>N_test && t(i) == 1)
-        corr_1 = corr_1 + 1;
-    end 
-end
-p_incorr_0 = (N_test - corr_0)/N_test
-p_incorr_1 = (N_test - corr_1)/N_test
-
-% Plot the decision boundary with the test samples
-figure(3); clf;
-scatter(Test_X_0(1,:), Test_X_0(2,:),'.b')
-hold on
-scatter(Test_X_1(1,:), Test_X_1(2,:),'.r')
-hold on
-contour(u,v,zp, [0, 0],'k', 'LineWidth', 2)
-title('Classification of ??? samples using the MAP rule')
-legend('Class 0','Class 1','Decision boundary')
-
-
-%% 4) Kernelized logistic regression
-
+% Create a set of training samples for future tasks
 N_training_samples = 1600;
-% Create a set of training samples
-rng(100)
+rng('default')
 X_0 = mvnrnd(m_0, C_0, N_training_samples/2)';
 X_1 = random(dist_1,N_training_samples/2)';
 X = [X_0 X_1];
 
-l = 0.1;
-lambda = 0.01;
+t_hat = classifyByMAP(X);
 
-a_26   = KLR(X, 26,  l, lambda);
-a_50   = KLR(X, 50,  l, lambda);
+% Plot the decision boundary with the test samples
+figure(3); clf;
+scatter(X_0(1,:), X_0(2,:),'.b')
+hold on
+scatter(X_1(1,:), X_1(2,:),'.r')
+hold on
+contour(u,v,z_MAP, [0, 0],'k', 'LineWidth', 2)
+title('Classification of 1600 samples using the MAP rule')
+legend('Class 0','Class 1','Decision boundary')
+
+
+%% 4) 5) 6) with: Kernelized Logistic Regression
+
+l = 0.5;
+lambda = 1000;
+
+%a_20   = KLR(X, 20,  l, lambda);
+%a_50   = KLR(X, 50,  l, lambda);
 a_100  = KLR(X, 100, l, lambda);
 a_200  = KLR(X, 200, l, lambda);
-%a_400  = KLR(X, 400, l, lambda);
+a_400  = KLR(X, 400, l, lambda);
 %a_800  = KLR(X, 800, l, lambda);
 %a_1600 = KLR(X_1600);
 
-%% 5) Plotting of data points and decision boundary with the kernel trick
+% Evaluate z = a'*K, calculate probabilities and plot decision boundary
+%[z_20 ] = classifyByKLR(X, 20,  a_20,  l); % N = 20
+%[z_50 ] = classifyByKLR(X, 50,  a_50,  l); % N = 50
+[z_100] = classifyByKLR(X, 100, a_100, l); % N = 100
+[z_200] = classifyByKLR(X, 200, a_200, l); % N = 200
+[z_400] = classifyByKLR(X, 400, a_400, l); % N = 400
+%[z_800] = classifyByKLR(X, 800, a_800, l); % N = 800
 
-% Evaluate z = a'*K
-[z_50 ] = evaluateZ(X, 50,  a_50,  l); % N = 50
-%[z_100] = evaluateZ(X, 100, a_100, l); % N = 100
-[z_200] = evaluateZ(X, 200, a_200, l); % N = 200
 
-
-%% 6)
-corr_0 = 0;
-corr_1 = 0;
-K_temp = zeros(length(X),1);
-zt = 0;
-for i=1:length(X)
-    for m=1:length(X)
-        K_temp(m) = exp(-norm(X(:,m)-X(:,i))^2/(2*l^2));
-    end
-    zt = a'*K_temp;
-    fprintf('Point nr %d at [%0.1f, %0.1f] has z = %0.2f\n',i,X(1,i),X(2,i),zt);
-    if     (zt < 0 && i <= (length(X)/2))
-        corr_0 = corr_0 + 1;
-    elseif (zt > 0 && i >  (length(X)/2))
-        corr_1 = corr_1 + 1;
-    end
-end
-p_incorr_0 = (length(X)/2 - corr_0)/(length(X)/2)
-p_incorr_1 = (length(X)/2 - corr_1)/(length(X)/2)
-
-%% 7)
+%% 7) = 4) 5) 6) with: Non-kernelized Logistic Regression
 
 % Map the data to 3rd degree feature vectors, gather in matrix form
-Phi = createFeatureMatrix([data_0 data_1]);
+Phi = createFeatureMatrix(X);
 w = zeros(size(Phi(:,1)));
 t = [zeros(1,N) ones(1,N)];
 
@@ -180,20 +141,20 @@ for k=1:50
         break
     end 
 end
-% Decision boundary
 
+% Decision boundary
 % Define the ranges of the grid
 u = linspace(-10, 10, 200);
 v = linspace(-10, 10, 200);
 
 % Initialize space for the values to be plotted
-zp = zeros(length(u), length(v));
+z_MAP = zeros(length(u), length(v));
 
 % Evaluate z = theta*x over the grid
 for i = 1:length(u)
     for j = 1:length(v)
         phi_uv = createFeatureMatrix([u(i) v(j)]');
-        zp(j,i) = w'*phi_uv;
+        z_MAP(j,i) = w'*phi_uv;
     end
 end
 
@@ -202,7 +163,7 @@ figure(2);
 scatter(data_0(1,:), data_0(2,:),'+')
 hold on
 scatter(data_1(1,:), data_1(2,:),'+r')
-contour(u,v,zp, [0, 0],'k', 'LineWidth', 2)
+contour(u,v,z_MAP, [0, 0],'k', 'LineWidth', 2)
 title('Classification from 200 samples using the MAP rule')
 legend('Class 0','Class 1','Decision boundary')
 
@@ -252,6 +213,6 @@ figure(1); clf
 scatter(MAP_test_samples_0(:,1), MAP_test_samples_0(:,2),'.')
 hold on
 scatter(MAP_test_samples_1(:,1), MAP_test_samples_1(:,2),'.r')
-contour(u,v,zp, [0, 0],'k', 'LineWidth', 2)
+contour(u,v,z_MAP, [0, 0],'k', 'LineWidth', 2)
 title('Simulated classification of 1000 samples using the MAP rule')
 legend('Class 0','Class 1','Decision boundary')
